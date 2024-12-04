93f30aefd11768f488dec66989b55381da748f62
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2024-12-04 12:36:33 +0000

    add model columns

--- pyterrier_rag/__init__.py
+++ pyterrier_rag/__init__.py
@@ -4,6 +4,7 @@ __version__ = '0.1.0'
 
 import pyterrier_rag._datasets
 import pyterrier_rag.measures
+import pyterrier_rag.model
 
 #from pyterrier_rag._datasets import MyAwesomeTransformer
 
---
+++ pyterrier_rag/model.py
@@ -0,0 +1,2 @@
+GOLD_COLUMNS = ['qid', 'golden_answers']
+ANSWER_COLUMNS = ['qid', 'qanswer']
