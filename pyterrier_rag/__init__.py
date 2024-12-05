d39caae53a936156a631a9420ee1d483a14dff9c
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2024-12-05 09:44:07 +0000

    add class for readers

--- pyterrier_rag/__init__.py
+++ pyterrier_rag/__init__.py
@@ -5,7 +5,7 @@ __version__ = '0.1.0'
 import pyterrier_rag._datasets
 import pyterrier_rag.measures
 import pyterrier_rag.model
-
+import pyterrier_rag.readers
 from pyterrier_rag._frameworks import Iterative
 
 __all__ = [
