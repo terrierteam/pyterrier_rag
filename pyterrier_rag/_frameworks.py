dd9f441c1622a2353ffd3f946d734e2f8962b2a8
“Andrew <a.parry.1@research.gla.ac.uk>
2025-02-05 13:05:41 +0000

    add iterative back (for now)

--- pyterrier_rag/_frameworks.py
+++ pyterrier_rag/_frameworks.py
@@ -126,3 +126,27 @@ class IRCOT(pt.Transformer):
             iter += 1
 
         return results
+
+
+class Iterative(pt.Transformer):
+
+    def __init__(self, retriever : pt.Transformer, reader : pt.Transformer, max_iter : Optional[int] =None):
+        self.retriever = retriever
+        self.reader = reader
+        self.max_iter = max_iter
+
+    @pta.transform.by_query(add_ranks=False)
+    def transform(self, inp : pd.DataFrame) -> pd.DataFrame:
+        iter = 1
+        stop = False
+        while not stop:
+            results = self.retriever(inp)
+            answers = self.reader(results)
+            # TODO is self.reader assumed to append LLM output to query?
+            if self.max_iter is not None and iter == self.max_iter:
+                stop = True
+            # TODO should be more customisable - perhaps a lambda?
+            if "the answer is" in answers.iloc[0].qanswer.lower():
+                stop = True
+            inp = answers
+        return answers
