723227da150389788e707ce1b04390b9e650724d
“Andrew <a.parry.1@research.gla.ac.uk>
2025-02-05 12:42:15 +0000

    Non-breaking change, addition of IRCOT

--- pyterrier_rag/readers/_content_aggregation.py
+++ pyterrier_rag/readers/_content_aggregation.py
@@ -1,22 +1,20 @@
-from itertools import chain
-from typing import List, Tuple, Union
+import pandas as pd
+from typing import List, Optional, Any, Union
+import pyterrier_alpha as pta
 
 
-def concat(context : Union[ List[str], List[Tuple[str]]],
-           tokenizer : str = None,
-           max_length : str = -1,
-           max_per_context : str = 512,
-           truncation_rate : int = 50) -> str:
-    """If tokenizer is not None,
-    add all context to total context
-    tokenize,
-    if it exceeds max_length,
-        truncate each context by 50 terms
-    """
+def concat(input_texts: List[str],
+           intermediate_format: Optional[callable] = None,
+           tokenizer: Any = None,
+           max_length: int = -1,
+           max_per_context: int = 512,
+           truncation_rate: int = 50) -> str:
     if tokenizer is not None:
         while True:
             total_context = ""
-            for c in context:
+            for c in input_texts:
+                if intermediate_format is not None:
+                    c = intermediate_format(c)
                 tokens = tokenizer.encode(c)
                 if len(tokens) > max_per_context:
                     tokens = tokens[:max_per_context]
@@ -27,8 +25,56 @@ def concat(context : Union[ List[str], List[Tuple[str]]],
             else:
                 max_per_context -= truncation_rate
     else:
-        total_context = "\n".join(chain(*context))
+        total_context = "\n".join(map(intermediate_format, input_texts))
     return total_context
 
 
-__all__ = ['concat']
+def dataframe_concat(input_frame: pd.DataFrame = None,
+                     tokenizer: Any = None,
+                     max_length: int = -1,
+                     max_per_context: int = 512,
+                     truncation_rate: int = 50,
+                     intermediate_format: Optional[callable] = None,
+                     relevant_fields: Optional[list] = ['text'],
+                     by_query: bool = False
+                     ) -> Union[str, pd.DataFrame, callable]:
+
+    def _concat(inp: pd.DataFrame) -> str:
+        max_context = max_per_context
+        if tokenizer is not None:
+            while True:
+                total_context = ""
+                for c in inp.itertuples():
+                    if intermediate_format is not None:
+                        c = intermediate_format({field: getattr(c, field, None) for field in relevant_fields})
+                    else:
+                        c = ' '.join([getattr(c, field, None) for field in relevant_fields])
+                    tokens = tokenizer.encode(c)
+                    if len(tokens) > max_per_context:
+                        tokens = tokens[:max_per_context]
+                        text = tokenizer.decode(tokens)
+                    total_context += text + "\n"
+                if len(tokenizer.encode(total_context)) <= max_length:
+                    break
+                else:
+                    max_context -= truncation_rate
+        else:
+            total_context = "\n".join(map(intermediate_format, inp))
+        return total_context
+
+    def per_query_concat(inp: pd.DataFrame) -> pd.DataFrame:
+        out = pta.DataFrameBuilder(['query_id', 'query', 'context'])
+        for qid, group in inp.groupby('query_id'):
+            out.extend({
+                'query_id': [qid],
+                'query': [group.iloc[0].query],
+                'context': [_concat(group)]
+            })
+        return out.to_df()
+
+    if by_query:
+        return per_query_concat(input_frame)
+    return _concat(input_frame)
+
+
+__all__ = ['concat', 'dataframe_concat']
