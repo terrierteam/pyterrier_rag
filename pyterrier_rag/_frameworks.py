1f8afc4ca25eb929a4d1407fe46b67ceb4754158
“Andrew <a.parry.1@research.gla.ac.uk>
2025-02-05 12:48:12 +0000

    non breaking change was in fact breaking

--- pyterrier_rag/_frameworks.py
+++ pyterrier_rag/_frameworks.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from functools import partial
-from prompts import template
+from outlines import prompt
 
 import pandas as pd
 import pyterrier as pt
@@ -19,14 +19,14 @@ ircot_system_message = 'You serve as an intelligent assistant, adept at facilita
 default_example = "Wikipedia Title: Kurram Garhi\nKurram Garhi is a small village located near the city of Bannu, which is the part of Khyber Pakhtunkhwa province of Pakistan. Its population is approximately 35000. Barren hills are near this village. This village is on the border of Kurram Agency. Other nearby villages are Peppal, Surwangi and Amandi Kala.\n\nWikipedia Title: 2001–02 UEFA Champions League second group stage\nEight winners and eight runners- up from the first group stage were drawn into four groups of four teams, each containing two group winners and two runners- up. Teams from the same country or from the same first round group could not be drawn together. The top two teams in each group advanced to the quarter- finals.\n\nWikipedia Title: Satellite tournament\nA satellite tournament is either a minor tournament or event on a competitive sporting tour or one of a group of such tournaments that form a series played in the same country or region.\n\nWikipedia Title: Trojkrsti\nTrojkrsti is a village in Municipality of Prilep, Republic of Macedonia.\n\nWikipedia Title: Telephone numbers in Ascension Island\nCountry Code:+ 247< br> International Call Prefix: 00 Ascension Island does not share the same country code( +290) with the rest of St Helena.\n\nQuestion: Are both Kurram Garhi and Trojkrsti located in the same country?\nThought: Kurram Garhi is located in the country of Pakistan. Trojkrsti is located in the country of Republic of Macedonia. Thus, they are not in the same country. So the answer is: no.\n\n"
 
 
-@template
+@prompt
 def ircot_prompt(reference: str, question: str) -> str:
-    return """{reference}Question: {question}\nThought:"""
+    """{reference}Question: {question}\nThought:"""
 
 
-@template
+@prompt
 def example_format(text: str, title: str = None) -> str:
-    return """
+    """
     {% if title != None %}
     Wikipedia Title: {{title}}
     {% endif %}
