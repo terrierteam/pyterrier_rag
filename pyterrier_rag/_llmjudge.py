b56978cb1f668c65534277f326a5524d48efb6ee
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-06 11:54:21 +0200

    minor updates to llmjudge

--- pyterrier_rag/_llmjudge.py
+++ pyterrier_rag/_llmjudge.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ir_measures
 import pyterrier as pt
 
 from pyterrier_rag.prompt._judge import (
@@ -7,31 +8,32 @@ from pyterrier_rag.prompt._judge import (
     PairwiseLLMJudgeSystemMessage,
     PointwiseLLMJudgeSystemMessage,
 )
+from pyterrier_rag.backend._base import Backend
 from pyterrier_rag.prompt import PromptTransformer
 
 
-def _pointwise_llm_judge(model):
+def _pointwise_llm_judge(backend):
     prompt = PromptTransformer(
         system_message=PointwiseLLMJudgeSystemMessage,
         instruction=PointwiseLLMJudgePrompt,
-        model_name_or_path=model.model_name_or_path,
+        model_name_or_path=backend.model_name_or_path,
         relevant_fields=["prediction", "gold"],
     )
-    return prompt >> model
+    return prompt >> pt.apply.generic(lambda x: backend.generate(x['query']))
 
 
-def _pairwise_llm_judge(model):
+def _pairwise_llm_judge(backend):
     prompt = PromptTransformer(
         system_message=PairwiseLLMJudgeSystemMessage,
         instruction=PairwiseLLMJudgePrompt,
-        model_name_or_path=model.model_name_or_path,
+        model_name_or_path=backend.model_name_or_path,
         relevant_fields=["prediction", "gold"],
     )
-    return prompt >> model
+    return prompt >> pt.apply.generic(lambda x: backend.generate(x['query']))
 
 
-def _make_pairwise_judge_function(model: pt.Transformer, minlabel: int = 3):
-    judge = _pairwise_llm_judge(model)
+def _make_pairwise_judge_function(backend: Backend, minlabel: int = 3):
+    judge = _pairwise_llm_judge(backend)
 
     def judge_fn(res: pd.DataFrame, qrels: pd.DataFrame):
         import pyterrier_alpha as pta
@@ -44,10 +46,21 @@ def _make_pairwise_judge_function(model: pt.Transformer, minlabel: int = 3):
         assert len(qrels), "No qrels found with minlabel of %d" % minlabel
 
 
-def _make_pointwise_judge_function(model: pt.Transformer, minlabel: int = 3):
-    judge = _pointwise_llm_judge(model)
+def _make_pointwise_judge_function(backend: Backend, minlabel: int = 3):
+    judge = _pointwise_llm_judge(backend)
 
     def judge_fn(res: pd.DataFrame, qrels: pd.DataFrame = None):
         import pyterrier_alpha as pta
 
         pta.validate.result_frame(res, extra_columns=["text"])
+        qrels = qrels[qrels.label >= minlabel]
+        qrels = qrels[qrels.label >= minlabel]
+        assert len(qrels), "No qrels found with minlabel of %d" % minlabel
+
+LLMJudge = ir_measures.define_byquery(
+    lambda qrels, res: max(
+        [judge(res.iloc[0], qrels) for judge in _make_pairwise_judge_function()]
+    ),
+    support_cutoff=False,
+    name="LLMJudge",
+)
