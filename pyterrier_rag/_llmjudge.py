eb8f7453a557f02cad04c909bf81cf1b6a8751ec
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-07 15:09:52 +0200

    update backend

--- pyterrier_rag/_llmjudge.py
+++ pyterrier_rag/_llmjudge.py
@@ -16,19 +16,19 @@ class BACKENDS(Enum):
     VLLM = "vllm"
     OPENAI = "openai"
 
-def get_backend(backend_type: str, model_name: str) -> Backend:
+def get_backend(backend_type: str, model_name: str, **backend_kwargs) -> Backend:
     """
     Get the backend object based on the backend type and model name.
     """
     if backend_type == BACKENDS.HF.value:
         from pyterrier_rag.backend._hf import HuggingFaceBackend
-        return HuggingFaceBackend(model_name)
+        return HuggingFaceBackend(model_name, **backend_kwargs)
     elif backend_type == BACKENDS.VLLM.value:
         from pyterrier_rag.backend._vllm import VLLMBackend
-        return VLLMBackend(model_name)
+        return VLLMBackend(model_name, **backend_kwargs)
     elif backend_type == BACKENDS.OPENAI.value:
         from pyterrier_rag.backend._openai import OpenAIBackend
-        return OpenAIBackend(model_name)
+        return OpenAIBackend(model_name, **backend_kwargs)
     else:
         raise ValueError(f"Unknown backend type: {backend_type}")
     
