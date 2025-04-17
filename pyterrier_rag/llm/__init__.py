5cc2ef1cfee1115d87d4130f1438a0c32b587e27
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-17 15:14:23 +0100

    eek

--- pyterrier_rag/llm/__init__.py
+++ pyterrier_rag/llm/__init__.py
@@ -1,18 +1,18 @@
-from ._base import Backend, BackendOutput, TextBackend, LogitBackend
-from ._hf import HuggingFaceBackend, CausalLMBackend, Seq2SeqLMBackend
-from ._openai import OpenAIBackend
-from ._vllm import VLLMBackend
-from ._util import get_backend
+from ._base import LLM, LLMOutput, TextLLM, LogitLLM
+from ._hf import HuggingFaceLLM, CausaLLM, Seq2SeqLLM
+from ._openai import OpenAILLM
+from ._vllm import VLLMLLM
+from ._util import get_LLM
 
 __all__ = [
-    "Backend",
-    "BackendOutput",
-    "TextBackend",
-    "LogitBackend",
-    "HuggingFaceBackend",
-    "CausalLMBackend",
-    "Seq2SeqLMBackend",
-    "OpenAIBackend",
-    "VLLMBackend",
-    "get_backend",
+    "LLM",
+    "LLMOutput",
+    "TextLLM",
+    "LogitLLM",
+    "HuggingFaceLLM",
+    "CausaLLM",
+    "Seq2SeqLLM",
+    "OpenAILLM",
+    "VLLMLLM",
+    "get_LLM",
 ]
