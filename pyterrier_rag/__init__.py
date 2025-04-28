37f7110de1075715fb393b1d9a7321db27562ec9
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-28 11:40:23 +0100

    done

--- pyterrier_rag/__init__.py
+++ pyterrier_rag/__init__.py
@@ -6,7 +6,6 @@ from pyterrier_rag import _datasets
 from pyterrier_rag import measures
 from pyterrier_rag import model
 from pyterrier_rag import readers
-from pyterrier_rag._frameworks import Iterative
 from pyterrier_rag.search_o1 import SearchO1
 from pyterrier_rag.search_r1 import SearchR1
 from pyterrier_rag.r1_searcher import R1Searcher
