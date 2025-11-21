f87bd919197f68b64e33a849de07ba8a285ba5bd
Zhili <zhili.shen17@gmail.com>
2025-11-21 15:12:14 +0000

    minor change for style check

--- pyterrier_rag/frameworks/agentic/__init__.py
+++ pyterrier_rag/frameworks/agentic/__init__.py
@@ -1,5 +1,12 @@
-from ._base import AgenticRAG# , HuggingfaceAgenticRAG
-
+from ._base import AgenticRAG
 from ._searchr1 import SearchR1
 from ._r1searcher import R1Searcher
 from ._searcho1 import SearchO1
+
+
+__all__ = [
+    "AgenticRAG",
+    "SearchR1",
+    "R1Searcher",
+    "SearchO1",
+]
